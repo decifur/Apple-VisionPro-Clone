fe285337cde8b5baf82cb8bac7f95647a4b68569
Jitesh Srivastav <90095692+decifur@users.noreply.github.com>
2023-08-21 22:35:34 +0530

    Added gsap scrollTrigger in page1

--- script.js
+++ script.js
@@ -62,3 +62,17 @@ gsap.to("#page-bottom",{
   },
   opacity:0
 })
+
+var tl = gsap.timeline({
+  scrollTrigger:{
+    trigger:`#page1`,
+    start: `top top`,
+    scrub: 1,
+    scroller: `#main`,
+    pin:true
+  }
+})
+
+tl.to("#page1>h1",{
+  top: `-50%`
+})
